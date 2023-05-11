d11a2715dd59e8dfd5155cdebe7a0954af306bf6
VaibhavShukla4 <96537580+VaibhavShukla4@users.noreply.github.com>
2023-05-11 14:53:09 +0530

    test-task

--- src/pages/AddUserData/index.js
+++ src/pages/AddUserData/index.js
@@ -67,7 +67,7 @@ const AddUserData = () => {
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
-      alert(JSON.stringify(values, null, 2));
+      console.log(JSON.stringify(values, null, 2));
     },
   });
   const [personalDetails, setPersonalDetails] = useState({
@@ -164,6 +164,7 @@ const AddUserData = () => {
     });
     result = await result.json();
     navigate("/");
+    // console.log(result);
   };
 
   return (
